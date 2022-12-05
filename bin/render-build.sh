63bdb5b9d82735ee860f92d7d7ae1ffe7bf20dec
Degradant <koshik.228@yandex.by>
2022-12-05 16:56:17 +0100

    change render-build.sh

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -1,14 +1,6 @@
-#!/usr/bin/env bash
-
-# exit on error
 set -o errexit
 
-# Use the correct version of Ruby
-source $RENDER_SRC_ROOT/web/bin/env.sh
-
-gem install bundler --silent
-gem update --system --silent --no-document
 bundle install
-
-npm install --prefix $RENDER_SRC_ROOT/web
-npm run build --prefix $RENDER_SRC_ROOT/web
+bundle exec rails assets:precompile
+bundle exec rails assets:clean
+bundle exec rails db:migrate
