f908acff7a38bcc461c21f54412e0736be02e885
Degradant <koshik.228@yandex.by>
2022-12-05 16:46:13 +0100

    add start.sh

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -1,6 +1,11 @@
 set -o errexit
 
-rvm use 3.0.2
+export RUBY_VERSION=$(cat $RENDER_SRC_ROOT/web/.ruby-version)
+export BUNDLE_GEMFILE=$RENDER_SRC_ROOT/web/Gemfile
+set_ruby_env $(fetch_or_build "$RUBY_VERSION")
+gem install bundler
+gem update --system
+
 bundle install
 bundle exec rails assets:precompile
 bundle exec rails assets:clean
