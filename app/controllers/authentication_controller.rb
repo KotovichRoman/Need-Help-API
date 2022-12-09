db819ba53d3a959e7b5f259fec75c3e89cb2aac5
Degradant <koshik.228@yandex.by>
2022-12-09 14:28:46 +0100

    minor fix

--- app/controllers/authentication_controller.rb
+++ app/controllers/authentication_controller.rb
@@ -4,7 +4,7 @@ class AuthenticationController < ApplicationController
     @user = User.find_by_email(params[:email])
     if @user&.authenticate(params[:password])
       token = JsonWebToken.encode(user_id: @user.id)
-      render json: { token: token, username: @user.login }, status: :ok
+      render json: { token: token, user: @user }, status: :ok
     else
       render json: { error: 'unauthorized' }, status: :unauthorized
     end
