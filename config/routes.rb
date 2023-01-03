c2e759860a39f87f9258c7c4ee6d6abfc3c8f6a9
Degradant <koshik.228@yandex.by>
2023-01-03 11:06:38 +0100

    minor fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -6,11 +6,11 @@ Rails.application.routes.draw do
 
   get "/requests-active", to: "requests#index_active_requests"
   get "/my-requests", to: "requests#index_my_requests"
-  get "/response-for-my-request", to: "responses#index_for_my_request"
 
   post "/user", to: "users#find_user_by_email"
   post "/auth/login", to: "authentication#login"
   post "/auth", to: "authentication#authorize_request"
+  post "/response-for-my-request", to: "responses#index_for_my_request"
   post "/approve_request/:id", to: "responses#approve_response"
   post "/decline_request/id", to: "responses#decline_response"
 end
