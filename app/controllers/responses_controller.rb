9e7caedd1a4d7d9b797e0d82b92714177045d72d
Degradant <koshik.228@yandex.by>
2023-01-03 10:26:50 +0100

    add approve requests

--- app/controllers/responses_controller.rb
+++ app/controllers/responses_controller.rb
@@ -1,10 +1,16 @@
 class ResponsesController < ApplicationController
-  before_action :set_response, only: %i[ show update destroy ]
+  before_action :set_response, only: %i[ show update destroy approve_response decline_response]
   before_action :authorize_request
 
   # GET /responses
   def index
     @responses = Response.all
+
+    render json: @responses
+  end
+
+  def index_for_my_request
+    @responses = Response.where(request_id: params[:request_id])
 
     render json: @responses
   end
@@ -39,6 +45,32 @@ class ResponsesController < ApplicationController
   # DELETE /responses/1
   def destroy
     @response.destroy
+  end
+
+  def approve_response
+    @response.is_approve = true
+
+    approve_user = User.find_by(id: @response.user_id)
+    approve_user.karma = approve_user.karma + 1
+
+    request = Request.find_by(id: @response.request_id)
+    request.is_confirm = true
+
+
+    Response.where(request_id: request.id, is_approve: false).each do |response|
+      response.is_approve = false
+      response.save
+    end
+
+    @response.save
+    approve_user.save
+    request.save
+  end
+
+  def decline_response
+    @response.is_approve = false
+
+    @response.save
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -10,4 +10,7 @@ Rails.application.routes.draw do
   post "/user", to: "users#find_user_by_email"
   post "/auth/login", to: "authentication#login"
   post "/auth", to: "authentication#authorize_request"
+  post "/response-for-my-request", to: "responses#index_for_my_request"
+  post "/approve_request/:id", to: "responses#approve_response"
+  post "/decline_request", to: "responses#decline_response"
 end
